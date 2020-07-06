fb83ebe19e7ac35c0132a753ba1eb4b0236bb7a8
Andre Jan <andrejanfebry17@gmail.com>
2020-07-06 21:35:29 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse vitae deserunt ratione! Quas nostrum veritatis inventore, rerum consequuntur exercitationem at in similique cumque earum animi libero eaque architecto, deleniti quibusdam.</p>
 </body>
 </html>
